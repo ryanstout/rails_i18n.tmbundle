b408230d9ae476ece61f31863cf75858b3005b89
Vincenzo Ruggiero <vincenzo.ruggiero@gmail.com>
2012-01-25 15:06:16 +0100

    Support for HAML processor

--- Support/lib/add_translation.rb
+++ Support/lib/add_translation.rb
@@ -16,6 +16,8 @@ class AddTranslation
     @selected_text = ENV['TM_SELECTED_TEXT']
     @path = ENV['TM_FILEPATH']
     
+    
+    
     get_token_key
     
     add_to_locale
@@ -29,7 +31,15 @@ class AddTranslation
     
     variable_str = (variables.size > 0) ? (', ' + variables.join(', ')) : ''
     
-    print "<%= t('.#{@token_key}'#{variable_str}) %>"
+    controller, file = @path.split(/\//)[-2..-1]
+    
+    cmd = "t('.#{@token_key}'#{variable_str})"
+    
+    if file.match(/.*\.html\.haml$/)
+      print "= #{cmd}"
+    else
+      print "<%= #{cmd}  %>"
+    end
   end
   
   # Ask the user for the token they want to use for this key
@@ -44,10 +54,10 @@ class AddTranslation
 
   def add_to_locale
     controller, file = @path.split(/\//)[-2..-1]
-
     # Remove partial _ and extensions from file url, or url
     if file
       file.gsub!('.html.erb', '')
+      file.gsub!('.html.haml', '')
       file.gsub!(/^[_]/, '')
     else
       # If controller
